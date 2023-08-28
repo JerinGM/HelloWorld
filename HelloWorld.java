161f13cf5a52386eb4432b5127405baaa9e189f1
Jerin Mathew <127280408+JerinGM@users.noreply.github.com>
2023-08-28 10:28:36 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//Java - First Code: HelloWorld
+public class HelloWorld
+{
+    // main method
+    public static void main(String[] args) {
+        //Printing Hello World
+        System.out.println("Hello World!");
+    } // main method ends
+} // Class ends here
